1570443ef749f535bdcbebc200b6c84263e9de3e
aizenzeb <aizenzeb@users.noreply.github.com>
2026-05-26 23:21:56 +0000

    Fix: Remove explicit debug signing config to use default

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,6 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
       storePassword = "android"
       keyAlias = "androiddebugkey"
       keyPassword = "android"
@@ -37,14 +35,13 @@ android {
   }
 
   buildTypes {
+    debug {
+    }
     release {
       isCrunchPngs = false
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
-    }
-    debug {
-      signingConfig = signingConfigs.getByName("debugConfig")
     }
   }
   compileOptions {
